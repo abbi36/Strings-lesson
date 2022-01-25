91edbe282584b5102bd2a0d3de1697067ebb234e
Abbi Engel <abbi.engel33@gmail.com>
2022-01-25 11:15:33 -0600

    strings lesson

---
+++ main.js
@@ -0,0 +1,21 @@
+
+    const myStr = "hello class";
+
+console.log(myStr.replace('c', 'C'));
+
+console.log(myStr.toUpperCase())
+
+console.log(myStr);
+
+console.log(myStr.repeat(5));
+
+console.log(myStr.replace('h', 'H'));
+
+console.log(myStr + '!');
+
+for (const element of myStr) {
+    console.log(element);
+}
+
+console.log(myStr.replace('h', 'H').replace ('c', 'C'));
+
